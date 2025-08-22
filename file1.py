2a5fb78db85a14d86932981ee9abfa6ce4a92b7c
Ghribi Oussama <137878019+OussamaGhribi@users.noreply.github.com>
2025-08-22 12:14:45 +0200

    conflict file1.py
    conflict

--- file1.py
+++ file1.py
@@ -1,7 +1,7 @@
 print("hello again")
 print("this is a hello from main (conflict)")
 
-print('deleted stufss')
+print('deleted stufss (conflict)')
 
 ch = "this a test to get all the updates from the main to the featA branch "
 print('deleted stufs')
